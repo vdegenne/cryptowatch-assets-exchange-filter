c5fdc91c3ec3b2ecc453b571594f72c20ccbf3c0
Valentin Degenne <vdegenne@gmail.com>
2020-11-16 15:41:53 +0100

    combine trading events

--- charts.js
+++ charts.js
@@ -3,16 +3,20 @@ class Ticker {
   lastPrice;
   shouldEmitSound = true;
 
-  static getTicker () {
+  static getTicker() {
     return document.querySelector('#price-ticker');
   }
 
-  constructor () {
+  updateTickerElement () {
+    this.element = Ticker.getTicker();
+  }
+
+  constructor() {
     // updater
     setInterval(() => {
-      this.element = Ticker.getTicker();
+      this.updateTickerElement();
       const value = this.getValue();
-      
+
       // emit sound when the value changes
       if (this.lastPrice && value !== this.lastPrice) {
         if (this.shouldEmitSound) {
@@ -24,8 +28,13 @@ class Ticker {
     }, 1000)
   }
 
-  getValue () {
+  getValue() {
     return this.element.innerText.trim();
+  }
+
+  click () {
+    this.updateTickerElement();
+    this.element.click();
   }
 }
 
@@ -33,4 +42,40 @@ function emitSound() {
   document.querySelector('#cuip-audio').play();
 }
 
-new Ticker;
+function selectCrossHair() {
+  document.querySelector('.icon-drawing-crosshair').dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
+}
+
+function registerEvents() {
+  window.addEventListener('keypress', function (e) {
+    if (e.key === 'f') {
+      ticker.click();
+      // prevent tool change in the chart frame
+      selectCrossHair();
+    }
+
+    if (e.key === 'F') {
+      document.querySelector('[data-testlabel="100%"]').click();
+    }
+
+    if (e.key === 'c') {
+      const order = document.querySelector('.order-close-button');
+      if (order) {
+        order.click();
+        selectCrossHair()
+      }
+
+      // setTimeout(selectCrossHair, 500)
+    }
+
+    if (e.key === 'b') {
+      document.querySelector('[data-testlabel="buy"]').click();
+    }
+    if (e.key === 's') {
+      document.querySelector('[data-testlabel="sell"]').click();
+    }
+  })
+}
+
+const ticker = new Ticker;
+registerEvents();
